bae40201e9d136956740b5c15c0790c809cd208f
enedilson-a11y <enedilson@theotitaconsultoria.com.br>
2026-06-27 22:33:59 -0300

    fix: ICMS40 em vez de ICMSSN400 (schema XSD)

--- server.js
+++ server.js
@@ -119,7 +119,7 @@ app.post('/emitir', async (req, res) => {
         indTot:   1
       },
       imposto: {
-        ICMS:   { ICMSSN400: { orig: 0, CSOSN: '400' } },
+        ICMS:   { ICMS40: { orig: 0, CST: '40' } },
         PIS:    { PISNT: { CST: '07' } },
         COFINS: { COFINSNT: { CST: '07' } }
       }
